051974d2a646bc3f7aa0829b3b97a61b2d7394cb
st0q <tssk713@gmail.com>
2022-07-13 23:21:00 +0900

    babelの設定を追加

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/** ↓ エディタで補完を効かせるための JSDoc */
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  module: {
+    rules: [
+      {
+        // 拡張子 js のファイル（正規表現）
+        test: /\.js$/,
+        // ローダーの指定
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
