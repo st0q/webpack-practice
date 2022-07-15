3cd6af1b835819e680a3b166a89dd9a6bf4c1774
st0q <tssk713@gmail.com>
2022-07-15 21:51:10 +0900

    ファイル名修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 /** ↓ エディタで補完を効かせるための JSDoc */
 /** @type {import('webpack').Configuration} */
 module.exports = {
+  entry: './src/index.jsx',
   devtool: "source-map",  // または 'inline-source-map' など
   devServer: {
     static: {
-      directory: "./dist",
-    },
+      directory: "./dist"
+    }
   },
   module: {
     rules: [
       {
-        // 拡張子 js のファイル（正規表現）
-        test: /\.js$/,
+        // 拡張子 js または jsx のファイル（正規表現）
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
         // ローダーの指定
         loader: "babel-loader",
-      },
-    ],
+      }
+    ]
+  },
+  // 依存関係解決に参照するファイルの拡張子を指定
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
 };
